7765f0ace7901dc81c326f395fe4d4f97dd1dcfd
Halfwhit <Va4r.c3y>
2019-03-15 05:23:08 +0000

    Started to work on the news article model

---
+++ database/migrations/2019_03_15_052019_create_news_articles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news_articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('image')->default('https://via.placeholder.com/1068x350?text=News+Article+Placeholder+Image');
+            $table->string('title');
+            $table->text('overview');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news_articles');
+    }
+}
