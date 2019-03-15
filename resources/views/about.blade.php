53c6259ae2e3be42187ad5d61ba6685ebc590234
halfwhit <halfwhit@gmail.com>
2019-03-15 13:45:14 +0000

    Added image of the business owner to the about page Added an embedded google map to the contact page Formatted the about and contact page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,16 +5,21 @@
 @section('content')
     <div class="card-deck">
         <div class="card">
+            <h5 class="card-header">About the company</h5>
             <div class="card-body">
-                <div class="card-title">About the company</div>
                 <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
             </div>
         </div>
         <div class="card">
+            <h5 class="card-header">About the owner</h5>
             <div class="card-body">
-                <div class="card-title">About the owners</div>
-                <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                <div class="card-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                </div>
             </div>
+        </div>
+        <div class="card">
+            <img src="{{asset('images/OwnerOne.jpg')}}" class="card-img-top small">
         </div>
     </div>
 
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,6 +3,7 @@
 @section('title', "Contact - Whittington's Wheels")
 
 @section('content')
+    <div class="row">
     <div class="card-deck">
         <div class="card">
             <div class="card-body">
@@ -15,6 +16,12 @@
                 <div class="card-title">Visit Us</div>
                 <div class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
             </div>
+        </div>
+    </div>
+    </div>
+    <div class="row my-5">
+        <div class="container text-center">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9878.93317762503!2d-0.2523808636818883!3d51.75620035576148!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48763c6246cb966f%3A0x8556a48775d046e4!2sUniversity+of+Hertfordshire+-+Science+Building!5e0!3m2!1sen!2suk!4v1552657241861" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
         </div>
     </div>
 @endsection
